0ce3b82f35063285f0a51bd26f44cdf0a2c08eb4
fabiobrasileiroo <fabio.h591@gmail.com>
2026-03-02 23:29:50 -0400

    [Fábio, Heloisa] :rotating_light: refactor: substituir let por const e melhorar comentário

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // 💡 DESAFIO: Deixe o código limpo!
-let pontos = 50;
+const pontos = 50;
 
-// exemplo de tratativa para verificar se a variável pontos é do tipo número e se tem o valor 50
-if(typeof pontos === "number") {
+// Verificar se a variável pontos é do tipo número e se tem o valor 50
+if (typeof pontos === "number") {
   console.log("O tipo de pontos é um número.");
 } else {
   console.log("O tipo de pontos não é um número.");
 }
+
 if (pontos === 50) {
   console.log("Você iniciou com 50 pontos!");
 }
